1291fe4836f2ef9153723ed944a30c9ab7b95c53
Suriya Kandaswamy <sardines007@gmail.com>
2015-01-18 19:07:12 -0500

    Peiyi's edits to the WebCrawler class with some GUI additions.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/70/30ac380a6f9f001411fbeaf849a113e6
@@ -0,0 +1,12 @@
+
+public class WebCrawler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- idt-webcrawler/src/WebCrawler.java
+++ idt-webcrawler/src/WebCrawler.java
@@ -1,12 +1,69 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class WebCrawler {
+/**
+ * This class will be a window that prompts the user for input fields.
+ */
+public class WebCrawler extends Frame implements ActionListener, WindowListener {
+	
+	public Panel insertURLPanel; //Container that has input and instructions for entering URL
+	public Label insertURLLabel; //Instructs users to enter the URL
+	public TextField insertURLField; //Field where users enter the URL
+	
+	//If there is any other input we need to take, add here
+	
+	public Button letsCrawlButton; //Once user is done, press this button to start crawling the website entered
+	
+	public WebCrawler () {
+		super("Web Crawler Application");
+		setLayout(new FlowLayout());
+		setSize(400, 200);
+		
+		//Initialize fields
+		insertURLPanel = new Panel();
+		insertURLLabel = new Label("Insert URL:");
+		insertURLField = new TextField("", 30);
+		letsCrawlButton = new Button("Crawl This Website");
+		
+		//Add items to JPanel
+		insertURLPanel.add(insertURLLabel);
+		insertURLPanel.add(insertURLField);
+		
+		//Add items to frame
+		add(insertURLPanel);
+		add(letsCrawlButton);
+		setVisible(true);
+		
+		//Add ActionListener
+		letsCrawlButton.addActionListener(this);
+	}
+
+	public static void main(String[] args) {
+		new WebCrawler();
+	}
 
 	/**
-	 * @param args
+	 * Listens for "Crawl" button click
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void actionPerformed(ActionEvent ae) {
+		//TODO Crawl the website
 	}
+	
+	/**
+	 * Close the window when close button is pressed.
+	 */
+	public void windowClosing (WindowEvent we) {
+		System.exit(0);
+	}
+
+	public void windowActivated(WindowEvent arg0) {}
+	public void windowClosed(WindowEvent arg0) {}
+	public void windowDeactivated(WindowEvent arg0){}
+	public void windowDeiconified(WindowEvent arg0) {}
+	public void windowIconified(WindowEvent arg0) {}
+	public void windowOpened(WindowEvent arg0) {}
 
 }
