a1d3fbe8ce0a70dbb803d88915b46aebf35fa8c7
peggy <peggy@192.168.2.210>
2015-01-25 13:56:46 -0500

    Changed window to JFrame subclass - still not closing D:

--- idt-webcrawler/src/WebCrawler.java
+++ idt-webcrawler/src/WebCrawler.java
@@ -1,40 +1,36 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
- * This class will be a window that prompts the user for input fields.
+ * This class will be a window that prompts the user for base URL.
  */
-public class WebCrawler extends Frame implements ActionListener, WindowListener {
+public class WebCrawler extends JFrame implements ActionListener {
 	
-	public Panel insertURLPanel; //Container that has input and instructions for entering URL
-	public Label insertURLLabel; //Instructs users to enter the URL
-	public TextField insertURLField; //Field where users enter the URL
+	private JPanel insertURLPanel; //Container that has input and instructions for entering URL
+	private JLabel insertURLLabel; //Instructs users to enter the URL
+	private JTextField insertURLField; //Field where users enter the URL
 	
-	//If there is any other input we need to take, add here
-	
-	public Button letsCrawlButton; //Once user is done, press this button to start crawling the website entered
+	private JButton letsCrawlButton; //Once user is done, press this button to start crawling the website entered
 	
 	public WebCrawler () {
 		super("Web Crawler Application");
-		setLayout(new FlowLayout());
-		setSize(400, 200);
+		setBounds(100, 100, 400, 200);
 		
 		//Initialize fields
-		insertURLPanel = new Panel();
-		insertURLLabel = new Label("Insert URL:");
-		insertURLField = new TextField("", 30);
-		letsCrawlButton = new Button("Crawl This Website");
+		insertURLPanel = new JPanel();
+		insertURLLabel = new JLabel("Insert URL:");
+		insertURLField = new JTextField("", 30);
+		letsCrawlButton = new JButton("Crawl This Website");
 		
 		//Add items to JPanel
 		insertURLPanel.add(insertURLLabel);
 		insertURLPanel.add(insertURLField);
 		
 		//Add items to frame
-		add(insertURLPanel);
-		add(letsCrawlButton);
+		Container con = this.getContentPane();
+		con.add(insertURLPanel);
+		con.add(letsCrawlButton);
 		setVisible(true);
 		
 		//Add ActionListener
@@ -51,19 +47,5 @@ public class WebCrawler extends Frame implements ActionListener, WindowListener 
 	public void actionPerformed(ActionEvent ae) {
 		//TODO Crawl the website
 	}
-	
-	/**
-	 * Close the window when close button is pressed.
-	 */
-	public void windowClosing (WindowEvent we) {
-		System.exit(0);
-	}
-
-	public void windowActivated(WindowEvent arg0) {}
-	public void windowClosed(WindowEvent arg0) {}
-	public void windowDeactivated(WindowEvent arg0){}
-	public void windowDeiconified(WindowEvent arg0) {}
-	public void windowIconified(WindowEvent arg0) {}
-	public void windowOpened(WindowEvent arg0) {}
 
 }
